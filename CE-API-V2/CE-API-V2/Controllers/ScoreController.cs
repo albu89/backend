8064c01b12f2e0fcee900122ec3a9b68beb2a1ff
Philipp Schlegel <Philipp.Schlegel@EXXETA.com>
2023-05-07 19:34:10 +0200

    CE-14_DCRM-77 Setup of the initial API Project

---
+++ CE-API-V2/CE-API-V2/Controllers/ScoreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace CE_API_V2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ScoreController : ControllerBase
+{
+    [HttpPost]
+    public async Task<string> Score(string name)
+    {
+        return $"Hello {name}!";
+    }
+}
