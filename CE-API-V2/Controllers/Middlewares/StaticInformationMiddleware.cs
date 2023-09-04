86abe0742debadfff0171ed5e23e3d808fb746ed
Kevin Sarmpinos <kevin.sarmpinos@exxeta.com>
2023-09-04 14:14:22 +0200

    Logo Urls for labelling and ifu-url added to every respsone header cherry-picked from b3ca798c5e9a1fc57631715a130651bfde6b33bf

--- CE-API-V2/Controllers/Middlewares/StaticInformationMiddleware.cs
+++ CE-API-V2/Controllers/Middlewares/StaticInformationMiddleware.cs
@@ -1,20 +1,26 @@
 ﻿using System.Text;
 using CE_API_V2.Constants;
+
 namespace CE_API_V2.Controllers.Middlewares;
 
 public class StaticInformationMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
 
-    public StaticInformationMiddleware(RequestDelegate next)
+    public StaticInformationMiddleware(RequestDelegate next, IConfiguration configuration)
     {
         _next = next;
+        _configuration = configuration;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-
         context.Response.Headers.Add("x-api-version", ApiVersion.CommitId);
+        context.Response.Headers.Add("ce-logo", "/images/CELogo.png");
+        context.Response.Headers.Add("ivd-logo", "/images/IVDLogo.png");
+        context.Response.Headers.Add("EcRepContactInfo", "/images/NemiusBanner.png");
+        context.Response.Headers.Add("ifu-url", _configuration.GetValue<string>("ifu-url"));
         // Call the next delegate/middleware in the pipeline.
         await _next(context);
     }
