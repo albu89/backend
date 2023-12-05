5fbb99b64185dd5f29839af200d43e55affb6391
Alessio Mascali <alessio.mascali@exxeta.com>
2023-12-05 09:16:21 +0000

    Merged in feature/Ce_79_additional_constraints_to_usertable (pull request #68)
    Added foreign key to billingsid in users table
    
    * Added foreign key to billingsid in users table
    
    
    Approved-by: Axel Kasper

---
+++ CE-API-V2/Migrations/20231205082143_CorrectedFaultyUserInputMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CE_API_V2.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectedFaultyUserInputMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_BillingId",
+                table: "Users",
+                column: "BillingId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Billings_BillingId",
+                table: "Users",
+                column: "BillingId",
+                principalTable: "Billings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Billings_BillingId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_BillingId",
+                table: "Users");
+        }
+    }
+}
